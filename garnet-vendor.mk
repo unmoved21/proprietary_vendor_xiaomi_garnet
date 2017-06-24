cefbf2a71011d89e47f55b0a938a53b2fe07afb1
Bruno Martins <bgcngm@gmail.com>
2017-06-24 09:10:22 +0100

    garnet: audio: Nuke dirac and misoundfx effect

--- garnet-vendor.mk
+++ garnet-vendor.mk
@@ -1745,7 +1745,6 @@ PRODUCT_PACKAGES += \
     sensors.touch.detect \
     sensors.ultrasoundproximity \
     libasphere \
-    libmisoundfx \
     libshoebox \
     vendor.display.color@1.0 \
     vendor.display.color@1.1 \
