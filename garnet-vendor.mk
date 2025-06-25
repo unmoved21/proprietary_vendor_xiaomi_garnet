cf821971d849740cd7b896d8860d70a4adff6159
unmoved21 <unmoved2121@gmail.com>
2025-06-25 18:09:44 +0200

    garnet: Declare EGL libs as symlinks during extraction
    Change-Id: I80f3850c7e865aed1120962db57b0db7b95a642e

--- garnet-vendor.mk
+++ garnet-vendor.mk
@@ -2186,7 +2186,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     system_ext_priv-app_ims_lib_arm64_libimscamera_jni_so \
     system_ext_priv-app_ims_lib_arm64_libimsmedia_jni_so \
-    system_ext_priv-app_WfdService_lib_arm64_libwfdnative_so
+    system_ext_priv-app_WfdService_lib_arm64_libwfdnative_so \
+    vendor_lib_libEGL_adreno_so \
+    vendor_lib_libGLESv2_adreno_so \
+    vendor_lib_libq3dtools_adreno_so \
+    vendor_lib64_libEGL_adreno_so \
+    vendor_lib64_libGLESv2_adreno_so \
+    vendor_lib64_libq3dtools_adreno_so
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
