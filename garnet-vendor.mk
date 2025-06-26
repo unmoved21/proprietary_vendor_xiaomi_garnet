b70ecbb97f0edc2840f725eb21e19110cc821538
unmoved21 <unmoved2121@gmail.com>
2025-06-26 03:00:41 +0200

    garnet: configs: Remove shoebox effect
    Change-Id: Ifb60563150ccd9c442941f3a6c05b07898807b19

--- garnet-vendor.mk
+++ garnet-vendor.mk
@@ -1744,7 +1744,6 @@ PRODUCT_PACKAGES += \
     sensors.touch.detect \
     sensors.ultrasoundproximity \
     libasphere \
-    libshoebox \
     vendor.display.color@1.0 \
     vendor.display.color@1.1 \
     vendor.display.color@1.2 \
